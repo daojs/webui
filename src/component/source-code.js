6321c42265b8638503440166a11ec2e7f4e50a7e
suro1995 <lxdyran@qq.com>
2018-04-22 19:25:23 +0800

    smaller bundle size

--- src/component/source-code.js
+++ src/component/source-code.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { vs } from 'react-syntax-highlighter/styles/hljs';
+
+// Standard build
+// import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+// import { vs } from 'react-syntax-highlighter/styles/prism';
+
+// Light build, 0.5M smaller bundle size
+import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/prism-light';
+import jsx from 'react-syntax-highlighter/languages/prism/jsx';
+import vs from 'react-syntax-highlighter/styles/prism/vs';
+
+registerLanguage('jsx', jsx);
 
 export default function SourceCode(props) {
   return (
     <div style={{ border: '1px solid #eee', borderTop: '0', lineHeight: '18px' }}>
       <SyntaxHighlighter
-        language="javascript"
+        language="jsx"
         style={vs}
         codeTagProps={{
-        style: {
-          display: 'block',
-          width: '100%',
-          wordBreak: 'break-all',
-          wordWrap: 'break-word',
-          whiteSpace: 'pre-wrap',
-        },
-      }}
+          style: {
+            display: 'block',
+            width: '100%',
+            wordBreak: 'break-all',
+            wordWrap: 'break-word',
+            whiteSpace: 'pre-wrap',
+          },
+        }}
       >
         {props.source}
       </SyntaxHighlighter>
