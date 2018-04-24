8c10352f8a6977cb3b33e74e9b357f7d9ef733ad
suro1995 <lxdyran@qq.com>
2018-04-24 13:36:37 +0800

    blacklist

--- src/repository.js
+++ src/repository.js
@@ -17,7 +17,7 @@ export function postComponent(options) {
 const getAllComponents = axios.get(`${SERVICE_URL}/list/@/`)
   .then(response => _.chain(response)
     .get('data.children', [])
-    .reject(item => _.includes(['base-chart', 'echarts-for-react', 'atlas-chart'], item))
+    .reject(item => _.includes(['base-chart', 'echarts-for-react', 'atlas-chart', 'chart-util'], item))
     .value())
   .catch(() => []);
 
