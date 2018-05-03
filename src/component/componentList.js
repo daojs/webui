e7c5f5cfdd9fa3294f7e61e81c84bbffd0735db5
suro1995 <lxdyran@qq.com>
2018-05-04 00:20:11 +0800

    renderCategoryFactory

--- src/component/componentList.js
+++ src/component/componentList.js
@@ -2,9 +2,21 @@ import React from 'react';
 import _ from 'lodash';
 import { Form, Icon, Menu } from 'antd';
 
+function renderCategoryFactory(items) {
+  return (Component, props) => (
+    <Component title={props.title} key={props.key}>
+      { items.filter(item => _.includes(item.category, props.key)).map(item => (
+        <Menu.Item key={item.name} >
+          <Icon type="dot-chart" />{item.name}
+        </Menu.Item>
+      )) }
+    </Component>
+  );
+}
 
 export default function (props) {
   const { items = [], total = items.length, showResults } = props;
+  const renderCategory = renderCategoryFactory(items);
 
   return (
     <Form
@@ -21,22 +33,15 @@ export default function (props) {
         mode="inline"
         defaultExpandAll
         defaultSelectedKeys={['0']}
+        // key is component name
         onClick={({ key }) => props.onSelect(key)}
-        onSelect={(e) => {
-          console.log(e);
-        }}
       >
         <Menu.SubMenu title="Layout" />
-        <Menu.SubMenu title="Container" />
+        { renderCategory(Menu.SubMenu, { title: 'Container', key: 'container' }) }
         <Menu.SubMenu title="Components" >
-          <Menu.ItemGroup title="Charts">
-            { items.filter(item => _.includes(item.category, 'chart')).map(item => (
-              <Menu.Item key={item.name} >
-                <Icon type="dot-chart" />{item.name}
-              </Menu.Item>
-            )) }
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Slicers" />
+          { renderCategory(Menu.ItemGroup, { title: 'Charts', key: 'chart' }) }
+          { renderCategory(Menu.ItemGroup, { title: 'Slicers', key: 'slicer' }) }
+          { renderCategory(Menu.ItemGroup, { title: 'Utilities', key: 'utility' }) }
         </Menu.SubMenu>
       </Menu>
     </Form>
